6a16f3654aa6c21c2c0f19b176d0787501ca769d
Eugen Lange <lange.e@mail.ru>
2023-02-04 23:38:41 +0500

    перерыв 1

---
+++ src/main/java/com/example/homework2_5collectionset/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.example.homework2_5collectionset.service;
+
+public interface EmployeeService {
+    public int printAllEmployee();
+    public int getCalculateSalarySum();
+    public int getCalculateAverageSalary();
+    public int getEmployeeMinSalary();
+    public double getEmployeeMaxSalary();
+
+}
---
+++ src/main/java/com/example/homework2_5collectionset/service/EmployeeServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.homework2_5collectionset.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public int printAllEmployee() {
+        return 0;
+    }
+
+    @Override
+    public int getCalculateSalarySum() {
+        return 0;
+    }
+
+    @Override
+    public int getCalculateAverageSalary() {
+        return 0;
+    }
+
+    @Override
+    public int getEmployeeMinSalary() {
+        return 0;
+    }
+
+    @Override
+    public double getEmployeeMaxSalary() {
+        return 0;
+    }
+}
