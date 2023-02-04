3fdd8919b8f92565d8bd7e555f631dae11ae5f30
Eugen Lange <lange.e@mail.ru>
2023-02-04 19:49:10 +0500

    initial commit

---
+++ src/main/java/com/example/homework2_5collectionset/Homework25CollectionSetApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework2_5collectionset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework25CollectionSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework25CollectionSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework2_5collectionset/Homework25CollectionSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_5collectionset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework25CollectionSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
